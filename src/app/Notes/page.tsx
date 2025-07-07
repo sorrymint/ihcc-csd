880282828df2f04ddde5e8c8b057a74b03244865
Caden Epp <ccepp2412@gmail.com>
2025-07-07 12:28:53 -0500

    added remark packages and imports for Notes Page

---
+++ src/app/Notes/page.tsx
@@ -0,0 +1,11 @@
+import { remark } from 'remark';
+import html from 'remark-html';
+
+export default function Notes() {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import './globals.css';
 
 // Load fonts with CSS variable support
 const geistSans = Geist({
