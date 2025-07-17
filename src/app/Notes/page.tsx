c294f9c52c43153daf7105fa622fd6f6227fb197
Caden Epp <ccepp2412@gmail.com>
2025-07-17 15:35:21 -0500

    added MDX files and tried to get it working but no luck sent errors in Teams

--- src/app/Notes/page.tsx
+++ src/app/Notes/page.tsx
@@ -1,11 +1,9 @@
-import { remark } from 'remark';
-import html from 'remark-html';
 
 export default function Notes() {
 
     return (
         <div>
-
+            
         </div>
     );
 }
---
+++ src/app/mdx-page/page.md
@@ -0,0 +1,9 @@
+
+
+# Hello World
+
+## Hello World
+
+### Hello World
+
+- this is a test file for rendering markdown files
