2ffb7a5fca7bd5b327b4e9df99fd0491617233fd
fiori-world <weldemichaelfiori@gmail.com>
2025-07-28 14:23:34 -0500

    Started the Header and need some more work

--- src/app/Componets/Header.tsx
+++ src/app/Componets/Header.tsx
@@ -1,9 +1,20 @@
-
+import Image from "next/image"
 
 export default function Header() {
     return (
         <div>
-            <h1>This is the Header Component</h1>
+            <div className="w-full h-20 bg-green-200 flex gap-30 items-center">
+                <div>
+                    <img src="../images/IHCClogoNew.png" alt="somgthing" width={50} height={100}/>
+                </div>
+                <ul className="flex gap-6 text-[20px] font-medium">
+                    <li>home</li>
+                    <li>Blog</li>
+                    <li>Notes</li>
+                    <li>Events</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
