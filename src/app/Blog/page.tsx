03bb8eb4897eaa2f5b9dd17160fcae9bac6d3cfc
fiori-world <weldemichaelfiori@gmail.com>
2025-07-30 13:01:32 -0500

    pushing header

---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page () {
+    return(
+        <div className="flex w-full justify-center">
+            <h1>This is the Blog Page</h1>
+        </div>
+    )
+}
--- src/app/Componets/Header.tsx
+++ src/app/Componets/Header.tsx
@@ -1,20 +1,36 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Header() {
     return (
         <div>
-            <div className="w-full h-20 bg-green-200 flex gap-30 items-center">
+            <div className="w-full h-20 flex gap-30 items-center justify-between px-10">
                 <div>
                     <img src="../images/IHCClogoNew.png" alt="somgthing" width={50} height={100}/>
                 </div>
                 <ul className="flex gap-6 text-[20px] font-medium">
-                    <li>home</li>
-                    <li>Blog</li>
-                    <li>Notes</li>
-                    <li>Events</li>
-                    <li>Contact</li>
+                    <li>
+                        <Link href="/">home</Link>
+                    </li>
+                    <li>
+                         <Link href="/Blog">blog</Link>
+                    </li>
+                    <li>
+                         <Link href="/Notes">notes</Link>
+                    </li>
+                    <li>
+                         <Link href="/Events">events</Link>
+                    </li>
+                    <li>
+                         <Link href="/Contact">contact</Link>
+                    </li>
+
                 </ul>
-            </div>
+            
+                <button className="bg-yellow-400 hover:bg-yellow-300 text-black font-semibold px-4 py-2 rounded-xl">
+                    <Link href="/login"> Login </Link>
+                </button>
+          </div>
         </div>
     )
 }
