54e3b3992cdc2dc66795d98ffa6100f0297e8c2c
laillah <laillah_zainath@stu.indianhills.edu>
2025-08-05 11:48:46 -0500

    This is the mission page

--- src/app/Our-Mission/page.tsx
+++ src/app/Our-Mission/page.tsx
@@ -1,13 +1,14 @@
-
-
+"use client";
 export default function OurMission() {
-
-
     return (
-        <div className="text-center m-[55px]">
-            <h1 className="text-8xl">
-                OUR MISSION PAGE COMING SOON
+        <div className="text-center m-[55px] bg-gray-200 p-10 rounded-xl shadow-lg">
+            <h1 className="text-8xl text-yellow-400">
+                Our mission
             </h1>
+            <br />
+            <p className="text-lg font-serif">
+                Our purpose is to promote social interaction, imagination, creativity, character development, team participation, problem solving, and conflict resolution techniques through Computer Programming.
+            </p>
         </div>
-    )
+    );
 }
